64420fe03c8363d11690037f381452dd9b449fcf
Saiful islam <saifulkapasia23@gmail.com>
2021-02-28 11:39:12 +0600

    component declared

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Basic React
-        </a>
+        
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+
       </header>
+    </div>
+  );
+}
+
+function Person(){
+  const personStyle={
+    color: 'red',
+    backgroundColor: 'yellow',
+    border: '30px solid white',
+    margin: '10px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name : Sakib Al Hasan</h1>
+      <h3>Job : Cricketer</h3>
     </div>
   );
 }
