fca4e63718b4b9ccfccf746702f401d4fba6daa4
Saiful islam <saifulkapasia23@gmail.com>
2021-02-28 21:48:55 +0600

    Load dynamic data, API call useEffect integrate state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -29,6 +29,7 @@ function App() {
           }
         </ul>
         <Counter></Counter>
+        <Users></Users>
         <p>I am a react Person</p>
         {
           products.map(pd => <Product product={pd}></Product>)
@@ -43,6 +44,25 @@ function App() {
       </header>
     </div>
   );
+}
+
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
 }
 
 function Product(props){
